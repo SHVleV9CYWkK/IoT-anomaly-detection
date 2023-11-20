fdfd46bc34edaf13cd38a7d0721f5a6a0001bcea
SHVleV9CYWkK <835862228@qq.com>
2023-11-21 00:39:56 +1100

    Completed the quantization code

--- raspberry_pi_inference.py
+++ raspberry_pi_inference.py
@@ -1,9 +1,10 @@
+import argparse
+import numpy as np
 import pandas as pd
 from sklearn.metrics import accuracy_score, precision_score, recall_score, f1_score
-from sklearn.preprocessing import MinMaxScaler
+import random
 import torch
 import torch.nn as nn
-import torch.nn.functional as F
 from tqdm import tqdm
 
 
@@ -32,6 +33,14 @@ class LightweightLSTM(nn.Module):
 
 if __name__ == '__main__':
     seed = 42
+    torch.manual_seed(seed)
+    np.random.seed(seed)
+    random.seed(seed)
+
+    parser = argparse.ArgumentParser(description='Run LSTM model with optional quantization.')
+    parser.add_argument('--quantize', action='store_true', help='Apply quantization to the model')
+    args = parser.parse_args()
+
     train_data = pd.read_csv('train_dataset.csv')
     test_data = pd.read_csv('test_dataset.csv')
 
@@ -52,6 +61,11 @@ if __name__ == '__main__':
     model.load_state_dict(torch.load('model_2023-11-20_00-57-22.pt'))
 
     model.eval()
+
+    if args.quantize:
+        print("Applying quantization")
+        torch.backends.quantized.engine = 'qnnpack'
+        model = torch.ao.quantization.quantize_dynamic(model, {nn.LSTM, nn.Linear}, dtype=torch.qint8)
 
     X_test_tensor = torch.tensor(X_test.to_numpy()).float().unsqueeze(1)
 
